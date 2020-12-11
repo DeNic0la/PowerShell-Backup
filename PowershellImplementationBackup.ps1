43715ef8fe9334cf7a1baeb1e3e042a7f1fcaac0
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2020-12-11 13:52:39 +0100

    some changes

--- PowershellImplementationBackup.ps1
+++ PowershellImplementationBackup.ps1
@@ -19,7 +19,7 @@ function doBackup(
 $SourcePath = 'C:\Backup\Source\', 
 $BackUpPath = 'C:\Backup\Backup\',
 $LogFilePath = 'C:\Backup\Log\log.txt',
-[bool]$WriteLogToHost = 'false'
+[bool]$WriteLogToHost = 1
 ){
     if (-not(Test-Path $LogFilePath -PathType Leaf)){
         New-Item -Path $LogFilePath -ItemType File
@@ -34,8 +34,40 @@ $LogFilePath = 'C:\Backup\Log\log.txt',
          LogMessage -Message "Ungültiges BackUp-Verzeichnis angegeben. Es wurde ein Neues erstellt" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath 
     }    
     LogMessage -Message "Alle Parameter sind Korrekt. Backup Wird gestartet" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath
-    Get-Item -Path $SourcePath| Get-ChildItem -Recurse | foreach { LogMessage -Message $_ -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath }
+    
+    #Starte Backup
 
+
+    Get-ChildItem -Path $SourcePath -Recurse | ForEach-Object {
+        if ($_.DirectoryName -eq $null){
+        LogMessage -Message "Ordner Entdeckt: $_" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath
+        }
+        else{
+        LogMessage -Message "Datei Entdeckt: $_" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath
+            Set-Location $SourcePath
+            $relativePath = Resolve-Path -Relative $_.Directory
+            Set-Location $BackUpPath
+            if (-not(Test-Path $relativePath)){
+                mkdir $relativePath
+                LogMessage -Message "Neuer Ordner erstellt: $relativePath" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath
+            }
+            if ($relativePath.StartsWith("..")){
+                $_ | Copy-Item -Destination $BackUpPath
+                LogMessage -Message "Datei Kopiert $_" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath
+            }
+            else{
+                $_ | Copy-Item -Destination $relativePath
+                LogMessage -Message "Datei Kopiert $_" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath
+            }
+            
+        }
+    }
+
+    #Get-Item -Path $SourcePath| Get-ChildItem -Recurse | foreach { LogMessage -Message $_ -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath }
+
+    LogMessage -Message "Das Backup wurde abgeschlossen" -WriteToLogFile $WriteLogToHost -LogPath $LogFilePath
+
+    Compare-Object -ReferenceObject (Get-ChildItem $SourcePath -Recurse) -DifferenceObject (Get-ChildItem $BackUpPath -Recurse) -Property Name,Length| Write-Host
 }
 
 # @Desc Macht das Backup
